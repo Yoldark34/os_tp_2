9a325002e2f7b2f4235dbce3cd7cf5e9bf47d436
Yoldark34 <yoldark@gmail.com>
2013-03-18 14:32:42 +0100

    First

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW_ELF-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW_ELF-Windows
+CND_ARTIFACT_NAME_Debug=os_tp_2
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW_ELF-Windows/os_tp_2
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW_ELF-Windows/package
+CND_PACKAGE_NAME_Debug=ostp2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW_ELF-Windows/package/ostp2.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW_ELF-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW_ELF-Windows
+CND_ARTIFACT_NAME_Release=os_tp_2
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW_ELF-Windows/os_tp_2
+CND_PACKAGE_DIR_Release=dist/Release/MinGW_ELF-Windows/package
+CND_PACKAGE_NAME_Release=ostp2.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW_ELF-Windows/package/ostp2.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
