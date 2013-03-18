0959922cb74d4f0b8fdfc2eab2bfecebe33a9b9e
Yoldark34 <yoldark@gmail.com>
2013-03-18 15:29:46 +0100

    Create type.h Add imdiskint.exe

---
+++ type.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   type.h
+ * Author: Yohan Sanchez (yohan.sanchez34@gmail.com)
+ *
+ * Created on 18 mars 2013, 15:25
+ */
+
+#ifndef TYPE_H
+#define	TYPE_H
+
+typedef unsigned char UCHAR;
+typedef unsigned char BYTE;
+typedef unsigned short UINT16;
+typedef unsigned int UINT32;
+
+
+#endif	/* TYPE_H */
+
